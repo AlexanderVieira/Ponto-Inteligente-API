91f7f2dae04b97c90499887a4890fe5f11adad53
AlexanderVieira <alexander.silva@al.infnet.edu.br>
2018-04-28 04:24:50 -0300

    Adicionado classes, enums no pacote model e dependência lombok.

---
+++ src/main/java/br/com/alex/pontointeligente/api/enums/PerfilEnum.java
@@ -0,0 +1,7 @@
+package br.com.alex.pontointeligente.api.enums;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USER;
+
+}
---
+++ src/main/java/br/com/alex/pontointeligente/api/model/Funcionario.java
@@ -0,0 +1,75 @@
+package br.com.alex.pontointeligente.api.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import br.com.alex.pontointeligente.api.enums.PerfilEnum;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "funcionario")
+public class Funcionario implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(name = "nome", nullable = false)
+	private String nome;
+
+	@Column(name = "email", nullable = false)
+	private String email;
+
+	@Column(name = "senha", nullable = false)
+	private String senha;
+
+	@Column(name = "cpf", nullable = false)
+	private String cpf;
+
+	@Column(name = "valor_hora", nullable = true)
+	private BigDecimal valorHora;
+
+	@Column(name = "qtd_horas_trabalho_dia", nullable = true)
+	private Float qtdHorasTrabalhoDia;
+
+	@Column(name = "qtd_horas_almoco", nullable = true)
+	private Float qtdHorasAlmoco;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "perfil", nullable = false)
+	private PerfilEnum perfil;
+
+	@Column(name = "data_criacao", nullable = false)
+	private Date dataCriacao;
+
+	@Column(name = "data_atualizacao", nullable = false)
+	private Date dataAtualizacao;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	private Empresa empresa;
+
+	@OneToMany(mappedBy = "funcionario", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<Lancamento> lancamento;
+
+}
